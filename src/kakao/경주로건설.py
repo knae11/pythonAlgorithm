c4fb99d1d0eaaaa37dd9c2e72e68e16f902d5c7c
Naeun <knae11@naver.com>
2021-02-10 22:31:07 +0900

    카카오 경주로 건설 중간시도 테스트케이스 39/100

--- src/kakao/경주로건설.py
+++ src/kakao/경주로건설.py
@@ -1,15 +1,15 @@
 import copy
 
+answer = 10e9
 
 def dfs(board, pos, n):
-    print(board, pos)
     x, y = pos[0][0], pos[0][1]
     prev = pos[1]
     cost = pos[2]
     board[x][y] = 1
     if pos[0] == (n, n):
-        print(pos)
-        return pos
+        global answer
+        answer = min(answer,pos[2] * 100)
     dx_s = [0, 0]
     dy_s = [1, -1]
     dx_c = [1, -1]
@@ -49,7 +49,6 @@ def dfs(board, pos, n):
 
 
 def solution(board):
-    answer = 0
     n = len(board)
     new_board = [[1] * (n + 2) for _ in range(n + 2)]
     for i in range(n):
@@ -57,11 +56,9 @@ def solution(board):
             new_board[i + 1][j + 1] = board[i][j]
     start_c = ((1, 1), 1, 0)
     start_s = ((1, 1), 0, 0)
-    s_result = dfs(copy.deepcopy(new_board), start_s, n)
-    print("-----------")
-    c_result = dfs(copy.deepcopy(new_board), start_c, n)
-    print(s_result, c_result)
-    return 0
+    dfs(copy.deepcopy(new_board), start_s, n)
+    dfs(copy.deepcopy(new_board), start_c, n)
+    return answer
 
 
 print(solution([[0, 0, 1, 0], [0, 0, 0, 0], [0, 1, 0, 1], [1, 0, 0, 0]]))
