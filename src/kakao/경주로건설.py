2726c4eff37591d8ed457cc1110276ee9fd23e88
Naeun <knae11@naver.com>
2021-02-11 18:55:55 +0900

    경주로건설: 47/100 안 풀린다..

--- src/kakao/경주로건설.py
+++ src/kakao/경주로건설.py
@@ -2,50 +2,30 @@ import copy
 
 answer = 10e9
 
-def dfs(board, pos, n):
+
+def dfs(board, pos, n, visited):
     x, y = pos[0][0], pos[0][1]
-    prev = pos[1]
+    dir = pos[1]
     cost = pos[2]
-    board[x][y] = 1
+    print(board, pos, visited)
+    global answer
     if pos[0] == (n, n):
-        global answer
-        answer = min(answer,pos[2] * 100)
-    dx_s = [0, 0]
-    dy_s = [1, -1]
-    dx_c = [1, -1]
-    dy_c = [0, 0]
-    # 가로
-    if prev == 0:
-        # 가로 가능
-        for i in range(2):
-            nx = x + dx_s[i]
-            ny = y + dy_s[i]
-            if board[nx][ny] == 0:
-                curr = ((nx, ny), 0, cost + 1)
-                dfs(board, curr, n)
-        # 세로
-        for i in range(2):
-            nx = x + dx_c[i]
-            ny = y + dy_c[i]
-            if board[nx][ny] == 0:
-                curr = ((nx, ny), 1, cost + 6)
-                dfs(board, curr, n)
-    # 세로
-    else:
-        # 세로
-        for i in range(2):
-            nx = x + dx_c[i]
-            ny = y + dy_c[i]
-            if board[nx][ny] == 0:
-                curr = ((nx, ny), 1, cost + 1)
-                dfs(board, curr, n)
-        # 가로
-        for i in range(2):
-            nx = x + dx_s[i]
-            ny = y + dy_s[i]
-            if board[nx][ny] == 0:
-                curr = ((nx, ny), 0, cost + 6)
-                dfs(board, curr, n)
+        answer = min(answer, pos[2] * 100)
+        print(answer)
+    direction = [0, 1, 2, 3]
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        nd = direction[i]
+        if board[nx][ny] == 0 and (nx, ny) not in visited:
+            if nd % 2 == dir % 2:
+                curr = ((nx, ny), dir, cost + 1)
+            if nd % 2 != dir % 2:
+                curr = ((nx, ny), nd, cost + 6)
+            visited.append((nx, ny))
+            dfs(board, curr, n, visited[:])
 
 
 def solution(board):
@@ -54,10 +34,11 @@ def solution(board):
     for i in range(n):
         for j in range(n):
             new_board[i + 1][j + 1] = board[i][j]
-    start_c = ((1, 1), 1, 0)
-    start_s = ((1, 1), 0, 0)
-    dfs(copy.deepcopy(new_board), start_s, n)
-    dfs(copy.deepcopy(new_board), start_c, n)
+    start_s = ((1, 1), 1, 0)
+    start_c = ((1, 1), 0, 0)
+    dfs(copy.deepcopy(new_board), start_s, n, [(1, 1)])
+    print("---")
+    dfs(copy.deepcopy(new_board), start_c, n, [(1, 1)])
     return answer
 
 
