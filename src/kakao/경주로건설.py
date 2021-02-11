ac645414a7aebc3289af70867b814b5bb2449784
Naeun <knae11@naver.com>
2021-02-11 19:29:35 +0900

    경주로건설: 87/100 인터넷 자바코드 참고,, 왜 안되지....

--- src/kakao/경주로건설.py
+++ src/kakao/경주로건설.py
@@ -3,15 +3,18 @@ import copy
 answer = 10e9
 
 
-def dfs(board, pos, n, visited):
+def dfs(board, pos, n, cost_board):
     x, y = pos[0][0], pos[0][1]
     dir = pos[1]
     cost = pos[2]
-    print(board, pos, visited)
     global answer
     if pos[0] == (n, n):
         answer = min(answer, pos[2] * 100)
-        print(answer)
+        return
+    if cost > answer:
+        return
+    if cost_board[x][y] < cost:
+        return
     direction = [0, 1, 2, 3]
     dx = [0, 1, 0, -1]
     dy = [1, 0, -1, 0]
@@ -19,13 +22,14 @@ def dfs(board, pos, n, visited):
         nx = x + dx[i]
         ny = y + dy[i]
         nd = direction[i]
-        if board[nx][ny] == 0 and (nx, ny) not in visited:
+        if board[nx][ny] == 0 and cost_board[nx][ny] >= cost:
             if nd % 2 == dir % 2:
                 curr = ((nx, ny), dir, cost + 1)
+                cost_board[nx][ny] = cost + 1
             if nd % 2 != dir % 2:
                 curr = ((nx, ny), nd, cost + 6)
-            visited.append((nx, ny))
-            dfs(board, curr, n, visited[:])
+                cost_board[nx][ny] = cost + 6
+            dfs(board, curr, n, cost_board)
 
 
 def solution(board):
@@ -36,9 +40,9 @@ def solution(board):
             new_board[i + 1][j + 1] = board[i][j]
     start_s = ((1, 1), 1, 0)
     start_c = ((1, 1), 0, 0)
-    dfs(copy.deepcopy(new_board), start_s, n, [(1, 1)])
-    print("---")
-    dfs(copy.deepcopy(new_board), start_c, n, [(1, 1)])
+    cost_board = [[10e9] * (n + 2) for _ in range(n + 2)]
+    dfs(new_board, start_s, n, cost_board)
+    dfs(new_board, start_c, n, cost_board)
     return answer
 
 
