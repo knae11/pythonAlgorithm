a994c7825df36fa8beeed8f12239dcc85b42eeeb
naeunkim <knae11@naver.com>
2021-03-11 08:13:44 +0900

    프린터: 풀이설명보충

--- src/programmerslevel2/프린터.py
+++ src/programmerslevel2/프린터.py
@@ -6,14 +6,16 @@ def solution(priorities, location):
 
     queue = deque()
     answer_list = []
-
+    # enumerate를 사용하면 간편하게 셋팅가능
     for i in range(len(priorities)):
         queue.append((priorities[i], i))
 
     while queue:
         candidate = queue.popleft()
+        # 이 문제의 경우 마지막까지 해당 번호가 나오지 않아서 queue가 비는경우를 확인해 주어야함!
         if not queue:
             return len(answer_list) + 1
+        # any로 체크 가능
         array = [item[0] for item in queue]
         if candidate[0] < max(array):
             queue.append(candidate)
