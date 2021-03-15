ae2d3a50c78ace2730a785c06e4ddd273c835079
Naeun <knae11@naver.com>
2021-03-15 17:07:48 +0900

    소수찾기: solved

---
+++ src/programmerslevel2/소수찾기.py
@@ -0,0 +1,29 @@
+from itertools import permutations
+import math
+
+
+def isPrime(primes, combi):
+    string = ""
+    for i in range(len(combi)):
+        if i == 0 and combi[i] == 0:
+            return 0
+        string += combi[i]
+    number = int(string)
+    if number in primes:
+        return 0
+    for i in range(2, int(math.sqrt(number)) + 1):
+        if number % i == 0:
+            return 0
+    else:
+        primes.add(number)
+        return 1
+
+
+def solution(numbers):
+    answer = 0
+    cards = [i for i in numbers]
+    primes = {0,1}
+    for i in range(1, len(numbers) + 1):
+        for combi in permutations(cards, i):
+            answer += isPrime(primes, combi)
+    return answer
