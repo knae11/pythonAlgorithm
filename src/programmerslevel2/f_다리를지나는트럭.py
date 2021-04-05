6848b1120a1764cdcb6c45dd143dc1385277041c
naeunkim <knae11@naver.com>
2021-04-05 15:09:46 +0900

    다리를 지나는 트럭: solved

--- src/programmerslevel2/f_다리를지나는트럭.py
+++ src/programmerslevel2/f_다리를지나는트럭.py
@@ -1,26 +1,28 @@
 from collections import deque
 
 
+# 다시 푼 문: queue의 고정크기 또는 0을 넣어서 채워주는 것이 포인트!!
+# 5번 시간초과: 매번 sum 계산 로직을 없애주니 통과!
 def solution(bridge_length, weight, truck_weights):
     answer = 0
-    truck_queue = deque(truck_weights)
-    bridge = deque()
-    # todo: bridge에 트럭이 올라온다 -> length 만큼 카운트
-    # 무게가 넘지 않고 공간이 비면 다음 차가 올라옴 +1
-
-    while truck_queue:
-        # bridge에 트럭을 올림
-        bridge.append(truck_queue.popleft())
-        answer += 1
-        # 무게가 괜찮다면 트럭을 더 올림
-        if sum(bridge) > weight:
-            truck_queue.appendleft(bridge[-1])
-            bridge.remove(bridge[-1])
+    truck_index = 0
+    bridge = deque([0 for _ in range(bridge_length)], maxlen=bridge_length)
+    total = 0
+    while bridge:
+        if truck_index < len(truck_weights):
+            next_truck = truck_weights[truck_index]
+            crossed_truck = bridge.popleft()
+            total -= crossed_truck
+            if total + next_truck > weight:
+                bridge.append(0)
+            else:
+                total += next_truck
+                bridge.append(next_truck)
+                truck_index += 1
+            answer += 1
+        else:
+            answer += len(bridge)
             break
-        print(answer, len(bridge))
-
-        bridge.clear()
-
     return answer
 
 
