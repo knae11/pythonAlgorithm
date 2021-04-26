71da3b5124775f66c4cc15b7c2d7ecc0c446014d
naeunkim <knae11@naver.com>
2021-04-26 13:00:10 +0900

    배달: solved

--- src/kakao/배달.py
+++ src/kakao/배달.py
@@ -4,17 +4,20 @@ def solution(N, road, K):
     answer = 0
     INF = 1e9
     graph = [[] for _ in range(N + 1)]
+    # 양방향 셋팅을 해주지 않으면 예상과 다른 결과가 나옴ㅠㅠ
     for conn in road:
         a = conn[0]
         b = conn[1]
         cost = conn[2]
+        # 양방향 셋팅을 해주지 않으면 예상과 다른 결과가 나옴ㅠㅠ
         graph[a].append((b, cost))
         graph[b].append((a, cost))
 
     distance = [INF] * (N + 1)
+    # 자기자신까지의 거리는 0으로 초기화해줘야 다 통과되었음;;
+    distance[1] = 0
     q = []
     heapq.heappush(q, (0, 1))
-
     while q:
         dist, now = heapq.heappop(q)
         if distance[now] < dist:
