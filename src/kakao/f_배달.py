f578531d46a2efc1c0f41c5993e3063fdfd494a0
naeunkim <knae11@naver.com>
2021-04-26 12:55:57 +0900

    배달: solving 87/100

---
+++ src/kakao/f_배달.py
@@ -0,0 +1,32 @@
+import heapq
+
+def solution(N, road, K):
+    answer = 0
+    INF = 1e9
+    graph = [[] for _ in range(N + 1)]
+    for conn in road:
+        a = conn[0]
+        b = conn[1]
+        cost = conn[2]
+        graph[a].append((b, cost))
+        graph[b].append((a, cost))
+
+    distance = [INF] * (N + 1)
+    q = []
+    heapq.heappush(q, (0, 1))
+
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+    for result in distance:
+        if result <= K:
+            answer += 1
+    return answer
+
+print(solution(5, [[1, 2, 1], [2, 3, 3], [5, 2, 2], [1, 4, 2], [5, 3, 1], [5, 4, 2]], 3))
