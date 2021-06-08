233474ba92dc399cf1742313c0480cef1d944425
naeunkim <knae11@naver.com>
2021-06-08 13:46:48 +0900

    큰수만들기: is solving (10 case failed)

--- src/programmerslevel2/f_큰수만들기.py
+++ src/programmerslevel2/f_큰수만들기.py
@@ -7,6 +7,8 @@ def solution(number, k):
     for i in range(len(number)):
         if remained_length - count <= 0:
             break
+        if count <= 0:
+            return answer
         f_list = numbers[:remained_length - count + 1]
         max_value = max(f_list)
         f_index = f_list.index(max_value)
