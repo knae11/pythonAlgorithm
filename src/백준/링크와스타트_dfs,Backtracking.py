2714abb408a937c95328864038c97050b852d4c0
naeunkim <knae11@naver.com>
2021-07-05 00:24:38 +0900

    링크와스타트: pypy3 solved(다른 풀이 추가)

--- src/백준/링크와스타트_dfs,Backtracking.py
+++ src/백준/링크와스타트_dfs,Backtracking.py
@@ -1,3 +1,4 @@
+
 import sys
 
 n = int(sys.stdin.readline().rstrip())
@@ -44,3 +45,34 @@ visited = [False for _ in range(n)]
 check(visited, n // 2, 0, 0)
 
 print(answer)
+
+'''
+# 다른사람 풀이 참고
+import sys
+from itertools import combinations
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+N = int(input())
+
+arr = [list(map(int, input().split())) for _ in range(N)]
+row = [sum(i) for i in arr]
+col = [sum(i) for i in zip(*arr)]
+print(arr, row, col)  # row-> 0,1,2,3 열의 합, col -> 0,1,2,3 행의 합
+new_arr = [i + j for i, j in zip(row, col)]  # 각 0,0 // 1,1 // 2,2 // 3,3 -> 열,행의 조합의 합
+total_sum = sum(new_arr) // 2
+print(new_arr, total_sum)
+result = float('inf')
+for num in range(1, N // 2 + 1):
+    for combi in combinations(new_arr, num):
+        result = min(result, abs(total_sum - sum(combi)))
+        if result == 0:
+            break
+    if result == 0:
+        break
+
+print(result)
+'''
