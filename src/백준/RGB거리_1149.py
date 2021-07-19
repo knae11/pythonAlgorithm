2be7a019425e74137be127dfc74b623d07a66bfd
naeunkim <knae11@naver.com>
2021-07-20 07:26:36 +0900

    rgb거리: solved
    - index 에러였음

--- src/백준/RGB거리_1149.py
+++ src/백준/RGB거리_1149.py
@@ -12,13 +12,13 @@ for i in range(n):
     line = list(map(int, input().split()))
     graph.append(line)
 
-answer = [[1e9] * n for _ in range(n)]
+answer = [[1e9] * 3 for _ in range(n)]
 
 answer[0] = graph[0]
 
 for row in range(1, n):
-    for col in range(n):
-        for prev_col in range(n):
+    for col in range(3):
+        for prev_col in range(3):
             if col == prev_col:
                 continue
             answer[row][col] = min(answer[row - 1][prev_col] + graph[row][col], answer[row][col])
