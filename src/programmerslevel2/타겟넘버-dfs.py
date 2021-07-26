e6b06fc2177206fbfa526757a64826319965a668
naeunkim <knae11@naver.com>
2021-07-27 08:04:38 +0900

    타겟넘버: solved(다시풀기)

--- src/programmerslevel2/타겟넘버-dfs.py
+++ src/programmerslevel2/타겟넘버-dfs.py
@@ -1,5 +1,5 @@
 # 인터넷 풀이 참고함.ㅠㅠㅠ
-
+'''
 answer = 0
 def dfs(numbers, target, process, index):
     global answer
@@ -17,5 +17,26 @@ def dfs(numbers, target, process, index):
 def solution(numbers, target):
     dfs(numbers, target, [], 0)
     return answer
+
+print(solution([1, 1, 1, 1, 1], 3))
+
+'''
+answer = 0
+
+
+def result_numbers(index, numbers, target, result):
+    global answer
+    if index >= len(numbers):
+        if result == target:
+            answer += 1
+        return
+    result_numbers(index + 1, numbers, target, result + numbers[index])
+    result_numbers(index + 1, numbers, target, result - numbers[index])
+
+
+def solution(numbers, target):
+    result_numbers(0, numbers, target, 0)
+    return answer
+
 
 print(solution([1, 1, 1, 1, 1], 3))
